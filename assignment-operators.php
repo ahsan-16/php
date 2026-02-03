1fc450d92d9cc6c874e877fa86a830e64744a9e3
Muhammad Ahsan <ahsanexpert16@gmail.com>
2026-02-03 14:26:39 +0500

    Assignment Operators

---
+++ assignment-operators.php
@@ -0,0 +1,32 @@
+<?php
+// Use of  Assignment Operators
+// Checkout All Assignment Operators
+// Example of Assignments Operators
+// Interview Question
+
+$a = 5;
+$b = 20;
+
+// $a=$b;
+
+// $a= $a+$b;
+// $a+=$b; //short form
+
+// $a= $a-$b;
+// $a -= $b; //short form
+
+// $a= $a*$b;
+// $a *= $b; //short form
+
+// $a= $a/$b;
+// $a/=$b; //short form
+
+// $a= $a%$b;
+// $a%=$b; //short form
+
+// $a= $a**$b;
+$a**=$b; //short form
+
+echo $a;
+
+?>
