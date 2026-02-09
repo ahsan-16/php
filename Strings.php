2de4e73f41a01aae8a3b78cb3411403662bd392a
Muhammad Ahsan <ahsanexpert16@gmail.com>
2026-02-09 15:17:00 +0500

    Strings and string Operators

---
+++ Strings.php
@@ -0,0 +1,32 @@
+<?php
+
+    // Strings and String Operators
+
+    // What is String?
+    // Use variables inside a string.
+    // Interview Questions
+
+    // There are two operators of strings
+    // 1: [ . ] dot operators / concate 
+    // 2: dot assignment operator / concatate
+
+    $a= "Muhammad Ahsan";
+    $b= 20;
+
+    // echo 'My name is '.$a; // dot operator
+
+    // $str = "Hello";
+    // $str1 = " How are you";
+
+    // echo $str .$str1;
+
+
+    $str = "Hello";    // dot assignment operator / concatate
+    $str1 = " How are you";
+    $str.=$str1;
+
+    echo $str;
+
+
+ 
+?>
