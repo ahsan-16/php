ad235380159602768ee22cb90b8d67449420d44b
Muhammad Ahsan <ahsanexpert16@gmail.com>
2026-02-09 12:11:34 +0500

    Comparison Operators

---
+++ Comparison-operators.php
@@ -0,0 +1,35 @@
+<?php
+ 
+    //Comparison Operators
+
+    // Use of Comparison Operators
+    // Checkout All Comparison Operators
+    // Example of comparison Operators
+    // Interview Questions
+
+    // When we comapre a number or a variable then we use Compariosn operators
+      
+      $x = 10;
+      $y = 11;
+    
+    //   var_dump($x==$y)  operator name "Equal"
+
+    // var_dump($x === $y) Operator name "Identical" This operator check value or type 
+
+    // var_dump($x!=$y) Operator name "Not Equal"
+
+    // var_dump($x<>$y) Operator Name "Not Equal"
+
+    // var_dump($x !== $y) Operator Name "Not Identical" 
+
+    // var_dump($x > $y) Operator Name "Greater than"
+
+    // var_dump($x < $y) Operator name "Less than"
+
+    // var_dump($x >= $y) Operator name "Greater than or equal to"
+
+    // var_dump($x <= $y) Operator Name "less than or equal to"
+
+    // var_dump($x <=> $y) Operator Name "SpaceShip" Returns an interger less than, equal to, or greater than zero, depending on if $x is less than, equal to, or greater than $y  
+      
+?>
