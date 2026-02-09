9523108c4391c7c3e2610e4b0ebab86ddce2983a
Muhammad Ahsan <ahsanexpert16@gmail.com>
2026-02-09 14:44:03 +0500

    Increment / Decrement

---
+++ Increment-decrement.php
@@ -0,0 +1,42 @@
+<?php
+   
+   // Increment / Decrement
+
+   // Use of these Operators
+   // checkout Increment / Decrement
+   // Example
+   // Interview Question
+
+    // ++$a Pre-increment Increment $a by one, then returns $a.
+    // $a++ Post-increment Returns $a, Then increments $a by one
+
+    //--$a Pre-decrement Decrements $a by one, then returns $a.
+    //$a-- post-decrement Returns $a, then decrements $a by one.
+
+
+     $a = 10;
+
+    //  echo ++$a;  Pre-increment
+
+    // echo $a++;  Post Increment
+
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+
+    // echo $a;
+    
+    // echo --$a; Pre-Decremrnt
+
+
+    // echo $a--; post-decrement
+
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+
+    // echo $a;
+    
+
+
+?>
